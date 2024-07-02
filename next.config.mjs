2c36555567ec9f4f6c6899faad4bc5b4956d17ae
axel <axel.benitez@santamariadelaluz.com>
2024-07-02 13:00:06 -0700

    prueba para mejorar cache

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-     // output: 'export'
-};
-
-export default nextConfig;
+     output: 'export',
+     async headers() {
+       return [
+         {
+           source: '/:path*', // Aplica a todas las rutas
+           headers: [
+             {
+               key: 'Cache-Control',
+               value: 'public, max-age=3600, must-revalidate', // Ajusta según tus necesidades
+             },
+           ],
+         },
+       ];
+     },
+   };
+   
+   export default nextConfig;
